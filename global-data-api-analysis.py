0b9108989cf08198f320aaa7aa1b5c78ca88ab5a
Aman Ali <pamanalionline@gmail.com>
2018-07-09 17:23:35 -0400

    Update global-data-api-analysis.py

--- global-data-api-analysis.py
+++ global-data-api-analysis.py
@@ -1,2 +1,9 @@
 #install "requests" using pip install requests
 #install "JSON" using pip install json
+#this api displays the global data found at the top of coinmarketcap.com
+import json #importing the libraries
+import requests #importing the libraries
+#giving the url link to a variable
+global_url = 'https://api.coinmarketcap.com/v2/global/'
+
+request = requests.get(global_url) #this fethes the URL and returns the data in JSON format, assigning it to 'request' variable
