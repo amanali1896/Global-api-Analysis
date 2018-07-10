d73e01d6aa62a009c891a49877936cb56aec6a42
Aman Ali <pamanalionline@gmail.com>
2018-07-09 20:40:26 -0400

    Update global-data-api-analysis.py

--- global-data-api-analysis.py
+++ global-data-api-analysis.py
@@ -7,7 +7,7 @@ from datetime import datetime #importing this so that we can have a proper time 
 #giving the url link to a variable
 global_url = 'https://api.coinmarketcap.com/v2/global/'
 
-request = requests.get(global_url) #this fethes the URL and returns the data in JSON format, assigning it to 'request' variable
+request = requests.get(global_url) #this fetches the URL and returns the data in JSON format, assigning it to 'request' variable
 results = request.json() #converts the variable to a json format, and that is how we expect the results to be shown.
 print(json.dumps(results, sort_keys=True, indent = 4))
 #.dumps converts the result to a string. sort_keys is made true so that the keys are coming in a sorted order
